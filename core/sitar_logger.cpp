32673617357a322e24978fc21b74610b8ff88c95
Neha Karanjkar <notpublic@notpublic.com>
2026-09-16 22:02:22 +0530

    Fix undefined logger::default_logstream in non-logging builds

--- core/sitar_logger.cpp
+++ core/sitar_logger.cpp
@@ -2,17 +2,15 @@
 #include "sitar_logger.h"
 #include<ostream>
 
-
-#ifdef SITAR_ENABLE_LOGGING
+//Definition of logger::default_logstream, a static member declared in
+//both the SITAR_ENABLE_LOGGING and non-logging variants of class
+//logger (sitar_logger.h) and referenced unconditionally by callers
+//(e.g. module::module(), sitar_default_main.cpp) -- so its storage
+//must exist unconditionally too, not just when logging is enabled.
 namespace sitar{
 
-			//definition of a static member variable 
-			//in logger class:
-			
 			//default ostream used by all modules for logging
 			std::ostream* logger::default_logstream;
 
 }
-
-#endif
 
